57267fde57ef128aa278d4a22d6b4229966d4ce4
vaishnavikadam1918 <vaishnavikadam8153@gmail.com>
2025-07-24 22:19:35 +0530

    Create showphoto.php
    sudo yum update -y
    sudo yum install -y httpd php
    sudo systemctl start httpd
    sudo systemctl enable httpd
    sudo nano /var/www/html/showphoto.php
    
         http://(IP address/showphoto.html

---
+++ showphoto.php
@@ -0,0 +1,12 @@
+#showphoto.php
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Show Photo</title>
+</head>
+<body>
+    <h1>Here is your photo:</h1>
+    <img src="https://as1.ftcdn.net/v2/jpg/05/40/92/36/1000_F_540923619_sMZStZE9HZjpecshSiMxcgLaUQzDnm5y.jpg" alt="Photo" style="max-width:500px;">
+</body>
+</html>
